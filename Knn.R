c61d5c5035bc2dd29a4dab59a028e398de0ea336
Nitya Verma <vrmnitya@gmail.com>
2024-04-07 13:23:53 +0530

    build the model

--- Knn.R
+++ Knn.R
@@ -15,3 +15,14 @@ test_size <- sample(nrow(data), 100)
 data_test <- data[test_size, ]
 
 data_train <- data[-test_size, ]
+
+#c) 
+
+library(class)
+  
+train_features = data_train[, 3:32]
+test_features = data_test[, 3:32]
+
+knn_model <- knn(train_features, test_features, cl = data_train$diagnosis, k = 21)
+knn_model
+
