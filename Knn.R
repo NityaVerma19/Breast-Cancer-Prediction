b2a07e0730b96ab134646ae8621bae645d8afd9b
Nitya Verma <vrmnitya@gmail.com>
2024-04-07 13:48:24 +0530

    build the model and find accuracy

--- Knn.R
+++ Knn.R
@@ -10,19 +10,23 @@ str(data)  #data type of each variable
 #b)Splitting the data
 
 set.seed(123)
-test_size <- sample(nrow(data), 100)
+test_size = sample(nrow(data), 100)
 
-data_test <- data[test_size, ]
+data_test = data[test_size, ]
 
-data_train <- data[-test_size, ]
+data_train = data[-test_size, ]
 
 #c) 
 
 library(class)
-  
-train_features = data_train[, 3:32]
-test_features = data_test[, 3:32]
+#scaling the data
+train_scale = scale(data_train[,3:32])
+test_scale = scale(data_test[, 3:32])
 
-knn_model <- knn(train_features, test_features, cl = data_train$diagnosis, k = 21)
+knn_model = knn(train_scale, test_scale, cl = data_train$diagnosis, k = 21)
 knn_model
 
+confusion_matrix = table(actual = data_test$diagnosis,predict =  knn_model)
+
+#Accuracy = (TP+TN)/(TP+TN_FP+FN)
+accuracy = sum(diag(confusion_matrix))/sum(confusion_matrix)
